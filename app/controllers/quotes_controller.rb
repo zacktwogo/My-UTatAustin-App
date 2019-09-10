b6cb4313b32f8cc4083b15f6fd93bf0db78e44ee
Zack V <zacktwogo@gmail.com>
2019-09-10 18:00:19 +0000

    Validating User Data, adding character limit for Contribute function

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,9 +8,12 @@ def index
   end
 
   def create
-    Quote.create(quote_params)
-    redirect_to root_path
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   end 
+   redirect_to root_path
+end
 
   private
 
